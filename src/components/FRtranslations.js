018e8ea696ddc3bb954600d72e060f042c66bcb4
Hazelstar <45568911+Hazelstar@users.noreply.github.com>
2020-01-09 13:44:31 +0000

    FR-Translation.js
    I forked the English Token Sale page to create a French version.

---
+++ src/components/FRtranslations.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default {
+  "fr": {
+    header: {
+      learnMore: "En Savoir Plus sur Kleros",
+      title: "Vente de jetons Kleros",
+      dates: "Début : 11 janvier 2020. Fin : 1er février 2020"
+    },
+    body: {
+      title: "Vente de PNK Kleros",
+      contribute: "Comment contribuer",
+      basicHeading: "Méthode simple",
+      basic: {
+        title: (<>Envoyez vos ETH directement à l adresse suivante<sup>*</sup></>),
+        copyContrib: "Copier l'adresse de contribution",
+        copied: "Copiée !",
+        disclaimer: (<><sup>*</sup>Les ordres les plus bas sont remplis en premier</>)
+      },
+      web3Heading: "Méthode Web3",
+      web3: {
+        title: "Faire une transaction avec un porte-monnaie Web3",
+        maxPriceLabel: "Prix maximum par PNK",
+        contributeLabel: "ETH en contribution",
+        total: "Total",
+        contribute: "Contribuer",
+        unlock: "Déverrouiller Metamask pour contribuer"
+      },
+      totals: {
+        title: "Au total, 150 Million de Pinakion (PNK) seront mis en vente au cours de la (des deux) prochaine(s) semaine(s).",
+        amountForSale: "Quantité mise en vente",
+        totalSold: "Quantité vendue",
+        remaining: "Quantité restant à vendre"
+      }
+    },
+    orders: {
+      title: "Ordres de vente",
+      sellOrder: "Ordre de vente",
+      price: "Prix (ETH)",
+      amount: "Quantité (PNK)"
+    }
+  }
+}
